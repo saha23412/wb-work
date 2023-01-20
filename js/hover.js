e2a518b7c93c68a26a9a9ebf64de00484448d2c0
Alexandr <k.a.r.2002@mail.ru>
2023-01-20 18:54:21 +0300

    icons

--- js/hover.js
+++ js/hover.js
@@ -3,19 +3,19 @@ const trashIcons = document.querySelectorAll('.trash')
 favoritesIcons.forEach(favorite => {
 
     favorite.addEventListener("mouseover", (event => {
-        favorite.src = '../images/icons/favorite_color.svg'
+        favorite.src = 'images/icons/favorite_color.svg'
     }))
     favorite.addEventListener("mouseout", (event => {
-        favorite.src = '../images/icons/favorites-icon.svg'
+        favorite.src = 'images/icons/favorites-icon.svg'
 
     }))
 })
 trashIcons.forEach(trash => {
     trash.addEventListener("mouseover", (event => {
-        trash.src = '../images/icons/trash_color.svg'
+        trash.src = 'images/icons/trash_color.svg'
     }))
     trash.addEventListener("mouseout", (event => {
-        trash.src = '../images/icons/trash-icon.svg'
+        trash.src = 'images/icons/trash-icon.svg'
 
     }))
 })
